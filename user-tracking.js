c51c3aecacad575a567bd54db3cc4ae4e8ee0e38
andrealikestomeow <andrea.w.060878@gmail.com>
2025-03-20 19:03:48 +0800

    Create user-tracking.js

---
+++ user-tracking.js
@@ -0,0 +1,28 @@
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i].trim();
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+// Check if user ID exists, otherwise generate one
+let userID = getCookie("userID");
+if (!userID) {
+    userID = "user_" + Math.floor(Math.random() * 999999);
+    setCookie("userID", userID, 365); // Store for 1 year
+}
+
+console.log("User ID:", userID);
