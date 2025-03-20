8d41db9bbd15785a7930a539de8c1fc344d60ee0
andrealikestomeow <andrea.w.060878@gmail.com>
2025-03-20 18:57:48 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC8sxEJKbDnBBWtnbwBHB0gEIrNbf0-kb8",
+    authDomain: "dengue-stagnant-water.firebaseapp.com",
+    projectId: "dengue-stagnant-water",
+    storageBucket: "dengue-stagnant-water.appspot.com",
+    messagingSenderId: "606657028114",
+    appId: "1:606657028114:web:9331950cc8e291a198f8cb"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
