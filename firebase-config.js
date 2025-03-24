4fe55f344756e7bd9a3f0f188bfc5e6607737729
andrealikestomeow <andrea.w.060878@gmail.com>
2025-03-24 20:41:28 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyBioGgdFP3CaadF9iW0EXBvHwMG1__iKr4",
     authDomain: "dengue-454102.firebaseapp.com",
     projectId: "dengue-454102",
-    storageBucket: "dengue-454102.appspot.com",
+    storageBucket: "dengue-454102.firebasestorage.app",
     messagingSenderId: "989474939574",
     appId: "1:989474939574:web:e1035d734e329efc997c17"
 };
